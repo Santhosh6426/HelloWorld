4ef551ab01d295d1822a3cd65568d08c907ed4c7
Santhosh V <santhosh.v6426@gmail.com>
2020-10-21 08:23:22 +0530

    Add project files.

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Class1
+    {
+        public static void Add(int a, int b) {
+            int c = a + b;
+            Console.WriteLine("The addition of two number is " + c);
+        
+        }
+
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the Value of a : ");
+            int a =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of b : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Class1.Add(5, 6);
+        }
+    }
+}
